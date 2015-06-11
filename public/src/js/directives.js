94cffea01c65b3a3e6407e75c0c1680ed93be219
dintorf <dintorf@gmail.com>
2015-06-10 21:03:26 -0700

    Added github and twitter to sidebar

--- public/src/js/directives.js
+++ public/src/js/directives.js
@@ -9,3 +9,56 @@ app.directive('bgImage', function(){
         });
     };
 });
+// <a class="twitter-timeline" href="https://twitter.com/dintorf" data-widget-id="608840345837641728">Tweets by @dintorf</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+/*
+*  AngularJS Directive for Twitter's Embedded Timeline with support for custom CSS.
+*  https://github.com/userapp-io/twitter-timeline-angularjs
+*/
+
+app.directive('twitterTimeline', [function() {
+		return {
+			restrict: 'A',
+			scope: {
+				cssUrl: "@",
+				autoResize: "="
+			},
+			link: function (scope, element, attrs) {
+				$('body').removeAttr('data-twttr-rendered');
+
+				element
+					.attr('id', 'twitter-feed')
+					.attr("width", "100%" || attrs.width)
+					.attr('data-chrome', 'noheader transparent')
+					.attr('data-widget-id', attrs.twitterTimeline)
+					.addClass('twitter-timeline');
+
+				function render() {
+					var body = $('.twitter-timeline').contents().find('body');
+
+					if (scope.cssUrl) {
+						body.append($('<link/>', { rel: 'stylesheet', href: scope.cssUrl, type: 'text/css' }));
+					}
+
+					function setHeight() {
+						if (body.find('.stream').length == 0) {
+							setTimeout(setHeight, 100);
+						} else {
+							body.find('.stream').addClass('stream-new').removeClass('stream').css('height', 'auto');
+							$('.twitter-timeline').css('height', (body.height() + 20) + 'px');
+						}
+					}
+
+					if (scope.autoResize) {
+						setHeight();
+					}
+				}
+
+				if (!$('#twitter-wjs').length) {
+					$.getScript((/^http:/.test(document.location)?'http':'https') + '://platform.twitter.com/widgets.js', function() {
+						render();
+						$('.twitter-timeline').load(render);
+	        		});
+				}
+			}
+		};
+	}]);
